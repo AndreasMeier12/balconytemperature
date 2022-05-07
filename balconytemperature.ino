e9039f2d5f9bf71dd8e4cebda51125fa280525e9
Andreas Meier <andreas.meier@stud.unibas.ch>
2022-05-07 08:55:51 +0200

    Change baud rate to 57600

--- balconytemperature.ino
+++ balconytemperature.ino
@@ -24,7 +24,7 @@ DHT_Unified dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(57600);
   Wire.begin();
 
   while (!Serial) {
